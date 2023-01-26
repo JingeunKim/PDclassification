560c8659987bc7891f11a1c4525eba02dbda9c0d
jingeunrad <wlsrms27@gmail.com>
2023-01-26 13:46:14 +0900

    updated

--- Model/preprocessing.py
+++ Model/preprocessing.py
@@ -5,7 +5,7 @@ def preprocess_inputscv_FS(df, i):
     df = df.copy()
     y = df['class']
     X = df.drop(['class'], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=i)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=i)
 
     return X_train, X_test, y_train, y_test
 
@@ -17,28 +17,17 @@ def preprocess_inputscv(df, i):
     x = df.iloc[:-1]
     X = x.transpose()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=i)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=i)
 
     return X_train, X_test, y_train, y_test
 
-def preprocess_inputscv_IG(df, i):
+def preprocess_inputscv_IG(df, i, j):
     df = df.copy()
     df = df.transpose()
 
-    y = df.loc[75]
+    y = df.loc[j]
     x = df.iloc[:-1]
     X = x.transpose()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=i)
-
-    return X_train, X_test, y_train, y_test
-
-def preprocess_inputscv_wolf(df, i):
-    df = df.copy()
-    df = df.transpose()
-
-    y = df.loc[8359]
-    x = df.iloc[:-1]
-    X = x.transpose()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=i)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=i)
 
     return X_train, X_test, y_train, y_test
